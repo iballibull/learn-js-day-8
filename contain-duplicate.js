6769d87cad3830cc3d6eee593f83d7900756a8b3
Muchammad Iqbal Yusuf <muchammadiqbalyusuf@gmail.com>
2024-07-31 19:14:06 +0700

    [Update] Solve logic test: contain duplicate of array

---
+++ contain-duplicate.js
@@ -0,0 +1,28 @@
+/**
+ * write a function that returns true if there's duplicate in the array, and false otherwise.
+ * SEE EXAMPLE BELLOW!
+ * 
+ * 
+Example
+console.log(containsDuplicate([1, 2, 3, 1])); // Output: true
+console.log(containsDuplicate([1, 2, 3, 4])); // Output: false
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])); // Output: true
+
+ * Determines if the array contains any duplicate value.
+
+ * @param {number[]} nums - The input array of integers.
+ * @return {boolean} Returns true if the array contains any duplicate value, false otherwise.
+ */
+
+function containsDuplicate(nums) {
+  for (let num of nums) {
+    if (nums.filter((value) => value === num).length > 1) {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(containsDuplicate([1, 2, 3, 1])); // Output: true
+console.log(containsDuplicate([1, 2, 3, 4])); // Output: false
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])); // Output: true
